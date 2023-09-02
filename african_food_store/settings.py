a48dd51d14107fbeb2705cc0afc4176724e7e2f8
KMaina-N <kelvinmaina350@gmail.com>
2023-09-02 14:36:37 +0200

    load statics

--- african_food_store/settings.py
+++ african_food_store/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-335bsyzx3%mr#kplch341gl$_c5&u(!$qyg_apu*bl9r8m37u_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
